84dd7dfd8e9665679413fc87308d7d297744fae9
Bryce McLachlan <brycemclachlan@gmail.com>
2021-04-26 20:35:56 +0000

    Inital boilerplate

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../scr/**/*.stories.mdx", "../scr/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-css-modules-preset",
+  ],
+};
