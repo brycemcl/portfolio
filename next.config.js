c3a79630c31448739d6fb426008fa9e06d996584
Bryce McLachlan <brycemclachlan@gmail.com>
2021-04-30 20:43:06 +0000

    added mdx support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+const withMDX = require('@next/mdx')({
+  options: {},
+  extension: /\.(md|mdx)$/,
+})
+module.exports = withMDX({
   future: {
     webpack5: true,
   },
-};
+})
